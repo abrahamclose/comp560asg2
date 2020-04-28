a0c75a22a1e96c8b0379e92390f3ea49debb88fe
Abraham Close <abraham@abrahamclose.com>
2020-04-28 14:50:42 -0400

    Initial Commit

---
+++ 560_asg_2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  560_asg_2
+//
+//  Created by Abraham Close on 4/28/20.
+//  Copyright © 2020 Abraham Close. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
